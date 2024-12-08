44f33ea2cc3ed1a345a623e65e9f9294387483cd
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 07:45:05 +0300

    Create app_colors_light.dart

---
+++ lib/core/theme/app_colors/app_colors_light.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColorsLight {
+  static const Color primaryColor = Color(0xFFFEFEFE);
+
+  static const LinearGradient lightGradient = LinearGradient(
+    colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+}
