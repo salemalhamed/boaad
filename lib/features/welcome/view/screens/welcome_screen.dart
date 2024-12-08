597c32bcd85c3022888f5e0392685296935ac1f9
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 07:45:20 +0300

    Create welcome_screen.dart

---
+++ lib/features/welcome/view/screens/welcome_screen.dart
@@ -0,0 +1,94 @@
+import 'package:boaad/core/theme/theme_controller.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  static const String routeName = '/welcome_screen';
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: ThemeController().themeGradient,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Image.asset(
+                "assets/images/lo.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Text(
+              ' اختر مسارك',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, "/test");
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromARGB(255, 186, 0, 0),
+                      ),
+                      height: 200,
+                      width: 145,
+                      child: Center(
+                        child: Text(
+                          "منتج",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 253, 253, 253),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, "/test");
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Color.fromARGB(255, 115, 178, 255)),
+                      height: 200,
+                      width: 145,
+                      child: Center(
+                        child: Text(
+                          "ممثل",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
