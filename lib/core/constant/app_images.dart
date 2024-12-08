cca3d47981f42d42645c321db9179b7e1e4eb358
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 10:38:36 +0300

    Delete app_images.dart

--- lib/core/constant/app_images.dart
+++
@@ -1,5 +0,0 @@
-class AppImages {
-  static const String appLogo = 'assets/images/logo/app_logo.png';
-  static const String appName = 'assets/images/logo/app_name.png';
-  static const String spotLight = 'assets/images/logo/spot_light.png';
-}
