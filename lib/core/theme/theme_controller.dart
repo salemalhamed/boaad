7503e86daaee66048335fd0c44d8025061817760
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 07:45:08 +0300

    Create theme_controller.dart

---
+++ lib/core/theme/theme_controller.dart
@@ -0,0 +1,6 @@
+import 'package:boaad/core/theme/app_colors/app_colors_light.dart';
+import 'package:flutter/material.dart';
+
+class ThemeController {
+  LinearGradient get themeGradient => AppColorsLight.lightGradient;
+}
