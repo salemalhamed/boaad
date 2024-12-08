5de1537d1a079e8e2b12688c681ecd36975b741f
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 07:44:49 +0300

    Create app_images.dart

---
+++ lib/core/constant/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String appLogo = 'assets/images/logo/app_logo.png';
+  static const String appName = 'assets/images/logo/app_name.png';
+  static const String spotLight = 'assets/images/logo/spot_light.png';
+}
