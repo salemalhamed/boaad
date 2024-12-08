3f111099eaacfa129bd9b5041bfd1d8c0d4f594d
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-12-08 07:45:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
-
-import 'package:boaad/core/constant/AppRoutes.dart';
+import 'package:boaad/core/constant/app_routes.dart';
+import 'package:boaad/features/splash/view/screens/splash_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:get/get.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    var mediaQueryData = MediaQuery.of(context);
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
     return GetMaterialApp(
       builder: DevicePreview.appBuilder,
       theme: ThemeData(useMaterial3: false),
       debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRoutes.generteRouts,
-      initialRoute: '/',
+      onGenerateRoute: AppRoutes.generatesRoutes,
+      unknownRoute: AppRoutes.unknownScreenGetPage,
+      initialRoute: SplashScreen.routeName,
+      getPages: AppRoutes.getPages,
     );
   }
 }
